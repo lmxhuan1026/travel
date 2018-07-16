72c83159860e34d585d096a16b0e50db129aa9d0
lukai <1050556089@qq.com>
2018-07-16 17:38:16 +0800

    单页面

---
+++ src/pages/list/List.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="home">list</div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List'
+}
+</script>
+
+<style>
+  .home {
+    font_size: 50px;
+  }
+</style>
